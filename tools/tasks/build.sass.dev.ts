acab3270f726e71233687b0810bb256df0f95ebe
Brijesh <brijesh.y.amin@gmail.com>
2016-02-06 22:11:06 +0530

    Removing sass-lint

--- tools/tasks/build.sass.dev.ts
+++ tools/tasks/build.sass.dev.ts
@@ -3,12 +3,12 @@
  */
 import {join} from 'path';
 import {APP_SRC, APP_DEST} from '../config';
-var scsslint = require('gulp-scss-lint');
+//var scsslint = require('gulp-scss-lint');
 
 export = function buildSassDev(gulp, plugins, option) {
     return function () {
         return gulp.src(join(APP_SRC, '**', '*.scss'))
-            .pipe(scsslint())
+            //.pipe(scsslint())
             .pipe(plugins.sass().on('error', plugins.sass.logError))
             .pipe(gulp.dest(APP_DEST));
     };
